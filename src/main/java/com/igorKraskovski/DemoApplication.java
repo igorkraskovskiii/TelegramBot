f34ff7cd265f1b2353fa11ad3a8b83f3c194e812
Alex Kolonitsky <alex@siryus.com>
2020-03-29 23:51:17 +0300

    added connection to telegram

--- src/main/java/com/igorKraskovski/DemoApplication.java
+++ src/main/java/com/igorKraskovski/DemoApplication.java
@@ -1,14 +1,24 @@
 package com.igorKraskovski;
 
+import com.igorKraskovski.telegram.Bot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiRequestException;
 
 @SpringBootApplication
 public class DemoApplication {
 
     public static void main(String[] args) {
         ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        Bot bot = new Bot();
+        try {
+            telegramBotsApi.registerBot(bot);
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
         SpringApplication.run(DemoApplication.class, args);
     }
 }
