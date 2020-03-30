8ec2e836cb8f27cec25cd8bf25d93caa6731621f
Alex Kolonitsky <alex@siryus.com>
2020-03-30 12:09:21 +0300

    refactoring code

--- src/main/java/com/igorKraskovski/telegram/Bot.java
+++ src/main/java/com/igorKraskovski/telegram/Bot.java
@@ -1,6 +1,7 @@
 package com.igorKraskovski.telegram;
 
 import com.igorKraskovski.entity.City;
+import org.springframework.beans.factory.annotation.Value;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
@@ -9,8 +10,8 @@ import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class Bot extends TelegramLongPollingBot {
 
-    private String botName = "ResCityInfoBot";
-    private String botToken = "1131663128:AAGS_aQCEBQJQcaLVW5GnTZJsxkZ1H13VlA";
+    private static final String  BOT_NAME = "ResCityInfoBot";
+    private static final String  BOT_TOKEN = "1131663128:AAGS_aQCEBQJQcaLVW5GnTZJsxkZ1H13VlA";
 
     Connect connect = new Connect();
 
@@ -18,14 +19,13 @@ public class Bot extends TelegramLongPollingBot {
         Message message = update.getMessage();
         sendText(message);
     }
+
     private synchronized void sendText(Message message) {
         SendMessage sendMessage = new SendMessage();
         sendMessage.enableMarkdown(true);
         sendMessage.setChatId(message.getChatId());
         sendMessage.setReplyToMessageId(message.getMessageId());
-
         City city = connect.getCityByCityName(message.getText());
-
         if (message.getText().equals("/start")) {
             sendMessage.setText("Hello " + message.getChat().getFirstName() +
                     "! :) \nThis bot will help you learn about any city." +
@@ -43,11 +43,12 @@ public class Bot extends TelegramLongPollingBot {
             e.printStackTrace();
         }
     }
+
     public String getBotUsername() {
-        return botName;
+        return BOT_NAME;
     }
 
     public String getBotToken() {
-        return botToken;
+        return BOT_TOKEN;
     }
 }
